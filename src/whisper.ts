6d2d24a9295b460ddce63d4b9e1c0f2a76ff7716
jrsarath <jrsarath@outlook.com>
2024-01-30 17:01:33 +0530

    Update whisper.ts

--- src/whisper.ts
+++ src/whisper.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import { promisify } from 'util'
 import { Params, Transcript, Whisper } from './types'
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Whisper: WhisperCTX } = require(
   path.join(__dirname, '../lib/whisper.cpp/build/Release/whisper-addon.node')
