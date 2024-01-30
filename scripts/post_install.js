57f6d8e5ab45c206de8215a7f8ff862bb58d7bdd
jrsarath <jrsarath@outlook.com>
2024-01-30 15:46:23 +0530

    Update post_install.js

--- scripts/post_install.js
+++ scripts/post_install.js
@@ -1,2 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const shell = require('shelljs')
+
+const dirPath = 'lib/whisper.cpp/build/Release'
+const file1 = 'whisper-addon.node'
+const file2 = 'libwhisper.dylib'
+
+const file1Exists = shell.test('-e', `${dirPath}/${file1}`)
+const file2Exists = shell.test('-e', `${dirPath}/${file2}`)
+
+if (!file1Exists || !file2Exists) {
+  shell.cd('lib/whisper.cpp/examples/addon.node')
+  shell.exec('npm install')
+  shell.exec('npm run build')
+}
