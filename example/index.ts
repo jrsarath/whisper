28a3169ce86249267c3ac20bc7646f4880b8d067
jrsarath <jrsarath@outlook.com>
2024-01-31 13:50:01 +0530

    refacotr

--- example/index.ts
+++ example/index.ts
@@ -2,5 +2,5 @@ import whisper from '../dist'
 
 whisper({
   model: '/Users/sarath/Documents/GitHub/Aura/models/whisper-medium.bin',
-  file: '/Users/sarath/Documents/GitHub/Aura/src/sports2.wav',
+  file: '/private/var/folders/g6/2zhj5zfj7cl8jwf3pmzb_tt40000gp/T/output.wav',
 }).then((r) => console.log(r))
